654b3c631549934105ceb373cb3d3fc03ae8c04b
S Turple <shawn.turple@gmail.com>
2026-05-04 21:43:05 -0700

    some roles and grants, passwords in env files, and re-enabled postgrest

--- migrations/V1__setup.sql
+++ migrations/V1__setup.sql
@@ -1,5 +1,11 @@
 CREATE DOMAIN "text/html" as text;
 
+-- Roles for postREST
+CREATE ROLE authenticator LOGIN NOINHERIT NOCREATEDB NOCREATEROLE NOSUPERUSER password '${REST_PASSWORD}';
+CREATE ROLE anonymous NOLOGIN;
+CREATE ROLE webuser NOLOGIN;
+grant webuser to authenticator;
+
 -- Roles
 create role web_admin NOLOGIN;
 create role web_editor NOLOGIN;
@@ -12,17 +18,18 @@ grant web_subscriber to web_editor;
 grant web_editor to web_admin;
 
 -- Test users
-create role test_admin  login password 'password';
-create role test_editor  login password 'password';
-create role test_subscriber  login password 'password';
-create role test_anon  login password 'password';
-
+create role test_admin  login password '${DB_USER_PASSWORD}';
+create role test_editor  login password '${DB_USER_PASSWORD}';
+create role test_subscriber  login password '${DB_USER_PASSWORD}';
+create role test_anon  login password '${DB_USER_PASSWORD}';
 
 
 grant web_admin to test_admin;
 grant web_editor to test_editor;
 grant web_subscriber to test_subscriber;
 grant web_anon to test_anon;
+
+select '${DB_USER_PASSWORD}' as test;
 
 CREATE TABLE users (
     id uuid primary key default gen_random_uuid(),
